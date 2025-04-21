3589a40e9c1f2a2ee6b97aa823ecf045925d902e
MateusCoppi <mateuscoppi1@gmail.com>
2025-04-21 15:52:39 -0300

    Ajustado funcao get_api para inserir os dados json diretamente no banco de dados

---
+++ scripts/extract_load/get_api.py
@@ -0,0 +1,54 @@
+import os
+import sys
+import requests
+from dotenv import load_dotenv
+from psycopg2.extras import Json
+
+load_dotenv()
+root_dir = os.getenv('ROOT_DIR')
+sys.path.append(root_dir)
+
+from scripts.database.postgres import PostgresConnection
+
+def extract_api(date: str):
+    '''
+    Extrai dados no formato json referente a transações pix da api do bacen.
+    
+    date: data base para extraaco dos dados
+    '''
+    
+    url = f"https://olinda.bcb.gov.br/olinda/servico/Pix_DadosAbertos/versao/v1/odata/EstatisticasTransacoesPix(Database=@Database)?@Database='{date}'&$format=json&$select=AnoMes,PAG_PFPJ,REC_PFPJ,PAG_REGIAO,REC_REGIAO,PAG_IDADE,REC_IDADE,NATUREZA,VALOR,QUANTIDADE"
+    r = requests.get(url=url)
+
+    if r.status_code == 200:
+        data = r.json()
+        print(f"Dados extraídos com sucesso: {r.status_code}")
+
+        registros = data.get("value", [])
+
+        conn = PostgresConnection()
+        conn.connect_pg()
+
+        if conn.connection is None:
+            print("Falha ao estalecer conexão com o banco")
+            return
+
+        # insere os dados no campo (dados) através do placeholder (%s)
+        # (%s) placeholder para o valor que será passadoo em seguida
+        # json(registros) converte o dict python para o formato json compativel com o postgres
+        with conn.connection.cursor() as cursor:
+            for data in registros:
+                cursor.execute(
+                    "INSERT INTO warehouse.estatisticas_pix (dados) VALUES (%s)",
+                    [Json(data)]
+                )
+        conn.connection.commit()
+        print(f"{len(registros)} registros inseridos com sucesso no banco.")
+
+    else:
+        print(f"Erro ao buscar os dados: {r.status_code}")
+
+
+if __name__ == "__main__":
+    DATE_API = '202412'
+    extract_api(date=DATE_API)
